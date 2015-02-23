3f8ecb1a6d71657b86dd1eb2a47be907a8b1f86e
bcsf11m059 <bcsf11m059@pucit.edu.pk>
2015-02-23 07:35:17 -0800

    My Last update

---
+++ Custom_Scripts/login_signup.js
@@ -0,0 +1,92 @@
+﻿$(document).ready(function () {
+
+    $('#signup-request').on('click', function () {
+        var name = $("#sn-name").val();
+        var email = $("#sn-email").val();
+        var pwd = $("#sn-password").val();
+        var i = 0;
+        
+        if (email.toString() == "" || pwd.toString() == "") {
+            $('form').attr('action', '');
+            if (email.toString() == "" )
+                $("#email_para").text("Email is missing, please try again");
+            if (pwd.toString() == "")
+                $("#pwd_para").text("Password missing, please try again");            
+        }
+    });
+
+   
+    $("#order").on('click', function () {
+        
+        var mobile = $("#mobile").val();
+        var card = $("#visa").val();
+        var address = $("#address").val();
+
+        if (mobile.length != 11 || card.length != 14 || address.length == 0) {
+            if (document.URL.lastIndexOf("wrongdetails") == -1)
+                var a = document.URL.replace("details", "wrongdetails");
+            else
+                a = document.URL;
+            $('form').attr('action', a);
+        }
+    });
+
+    $("#check-request").on('click', function () {
+        
+        var name = $("#sn-name").val();
+        $.getJSON('/Login/checkUserName?Name=' + name, function (data) {            
+            $("#signup_para").text(data);
+        });
+    });
+
+    $("#src-request").on('click', function () {
+        var type = $("#src-type").val();
+        var cat = $("#src-cat").val();        
+        $.getJSON('/Search/SearchByCatagoryAndName?Type=' + type + '&' + 'catagory=' + cat, function (data) {
+            
+            if (data.length == 0) {
+                $("#list").append(
+                    "<li>" +
+                        "<h2>" + "No serach results found" + "</h2>" +
+                     "</li>"
+                );
+            }
+            else {
+                $.each(data, function (id, item) {
+                    $("#list").append(
+                        "<li>" +
+                            data[id].name
+                        + "</li>"
+                    );
+                });
+            }
+        });
+    });
+
+    $("#src-request-price").on('click', function () {
+        var min = $("#src-min").val();
+        var max = $("#src-max").val();
+        
+        $.getJSON('/Search/SearchByPrice?min=' + min + '&' + 'max=' + max, function (data) {           
+            if ( data.length == 0 ) {
+                $("#list").append(
+                    "<li>" +
+                       "<h2>" + "No serach results found" + "</h2>" +
+                     "</li>"
+                );
+            }
+            else {
+                $.each(data, function (id, item) {
+                    $("#list").append(
+                        "<li>" +
+                            data[id].name
+                        + "</li>"
+                    );
+                });
+            }
+           
+        });
+        
+    });
+
+});
---
+++ Dropdown_Example_Code/Readme.txt
@@ -0,0 +1,20 @@
+﻿
+Add this code below to your view to get the dropdown up and running:
+
+<div class="dropdown">
+            <button type="button" class="dropdown-button dropdown-toggle" data-toggle="dropdown">-Select Unit-<span class="caret"></span></button>
+            <ul class="dropdown-menu" role="menu" style="">
+                <li><a href="#stay" class="change">-Select Unit-</a></li>
+                <li><a href="#stay" class="change">D<sub>10</sub></a></li>
+                <li><a href="#stay" class="change">D<sub>20</sub></a></li>
+                <li><a href="#stay" class="change">D<sub>30</sub></a></li>
+                <li><a href="#stay" class="change">D<sup>10</sup></a></li>
+                <li><a href="#stay" class="change">D<sup>20</sup></a></li>
+                <li><a href="#stay" class="change">D<sup>30</sup></a></li>
+                <li><a href="#stay" class="change">M<sup>2</sup></a></li>
+                <li><a href="#stay" class="change">H<sub>2</sub>O</a></li>
+                <li><a href="#stay" class="change">H<sub>z</sub></a></li>
+                <li><a href="#stay" class="change">Sub<sub>100</sub></a></li>
+                <li><a href="#stay" class="change">Sup<sup>100</sup></a></li>
+            </ul>
+        </div>
